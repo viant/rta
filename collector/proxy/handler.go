2f4d33bea0421e1eb9095440e6624d25ab25e56a
Michał Filipowicz <mfilipowicz@interia.eu>
2023-08-01 21:43:31 +0200

    ENG-38706 updated proxy Handle func

--- collector/proxy/handler.go
+++ collector/proxy/handler.go
@@ -8,6 +8,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"reflect"
+	"sync"
+	"unsafe"
 )
 
 type Handler struct {
@@ -39,7 +41,9 @@ func (h *Handler) buildResponse(writer http.ResponseWriter, err error) {
 	writer.Write(data)
 }
 
-func (h *Handler) Handle(data []byte) error {
+const collectorThread = 4
+
+func (h *Handler) Handle(data []byte) (err error) {
 	sliceValuePtr := reflect.New(h.targetType)
 	request := &Request{}
 	request.Records = sliceValuePtr.Interface()
@@ -48,13 +52,67 @@ func (h *Handler) Handle(data []byte) error {
 	}
 	slicePtr := xunsafe.AsPointer(sliceValuePtr.Interface())
 	xSlice := xunsafe.NewSlice(h.targetType, slicePtr)
-	for i := 0; i < xSlice.Len(slicePtr); i++ {
+	sliceLen := xSlice.Len(slicePtr)
+	threadRanges := partedSliceUpperBounds(sliceLen, collectorThread)
+	wg := sync.WaitGroup{}
+	wg.Add(len(threadRanges))
+	lowerBound := 0
+	for j := 0; j < len(threadRanges); j++ {
+		upperBound := threadRanges[j]
+		if j > 0 {
+			lowerBound = threadRanges[j-1]
+		} else {
+			lowerBound = 0
+		}
+
+		go func(lBound, uBound int) {
+			defer wg.Done()
+			cErr := h.collectinBackground(lBound, uBound, xSlice, slicePtr)
+			if cErr != nil {
+				err = cErr
+			}
+		}(lowerBound, upperBound)
+	}
+
+	wg.Wait()
+	return err
+}
+
+func (h *Handler) collectinBackground(lowerBound int, upperBound int, xSlice *xunsafe.Slice, slicePtr unsafe.Pointer) error {
+	for i := lowerBound; i < upperBound; i++ {
+
 		record := xSlice.ValuePointerAt(slicePtr, i)
 		if err := h.collector.Collect(record); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+// partedSliceUpperBounds produces slice of upper bounds for given slice length and divisor
+// We assume that every part of slice will have len > 0
+func partedSliceUpperBounds(sliceLen int, divisor int) []int {
+	if divisor == 0 {
+		divisor = 1
+	}
+
+	delta := sliceLen / divisor
+	partsCnt := divisor
+	if delta < 1 {
+		delta = 1
+		partsCnt = sliceLen
+	}
+
+	var ranges = make([]int, partsCnt)
+
+	for i := 1; i <= partsCnt; i++ {
+		upperBound := i * delta
+		if i == partsCnt {
+			upperBound = sliceLen
+		}
+		ranges[i-1] = upperBound
+	}
+	return ranges
 }
 
 func (h *Handler) readPayload(request *http.Request) ([]byte, error) {
--- collector/proxy/handler_test.go
+++ collector/proxy/handler_test.go
@@ -1,11 +1,11 @@
-package proxy_test
+package proxy
 
 import (
 	"fmt"
 	"github.com/francoispqt/gojay"
 	"github.com/stretchr/testify/assert"
-	"github.com/viant/rta/collector/proxy"
 	"reflect"
+	"sync"
 	"testing"
 )
 
@@ -36,15 +36,171 @@ func TestHandler_Handle(t *testing.T) {
 			},
 			collector: &recordCollector{collection: map[string]float32{}},
 		},
+		{
+			description: "simple agg empty data",
+			data: `{
+"BatchID":"123",
+"Records":[]
+}`,
+			expect:    map[string]float32{},
+			collector: &recordCollector{collection: map[string]float32{}},
+		},
+		{
+			description: "simple agg 1 row",
+			data: `{
+"BatchID":"123",
+"Records":[
+	{"Key":"1", "Amount":1.2},
+]
+}`,
+			expect: map[string]float32{
+				"1": 1.2,
+			},
+			collector: &recordCollector{collection: map[string]float32{}},
+		},
+		{
+			description: "simple agg 4 rows ",
+			data: `{
+"BatchID":"123",
+"Records":[
+	{"Key":"1", "Amount":1.2},
+	{"Key":"2", "Amount":2.2},
+	{"Key":"3", "Amount":3.3},
+	{"Key":"1", "Amount":10.2}
+]
+}`,
+			expect: map[string]float32{
+				"1": 11.4,
+				"2": 2.2,
+				"3": 3.3,
+			},
+			collector: &recordCollector{collection: map[string]float32{}},
+		},
+		{
+			description: "simple agg 7 rows ",
+			data: `{
+"BatchID":"123",
+"Records":[
+	{"Key":"1", "Amount":1.2},
+	{"Key":"2", "Amount":2.2},
+	{"Key":"3", "Amount":3.3},
+	{"Key":"1", "Amount":9.2},
+	{"Key":"1", "Amount":1.1},
+	{"Key":"2", "Amount":2.2},
+	{"Key":"3", "Amount":3.3}
+]
+}`,
+			expect: map[string]float32{
+				"1": 11.5,
+				"2": 4.4,
+				"3": 6.6,
+			},
+			collector: &recordCollector{collection: map[string]float32{}},
+		},
+		{
+			description: "simple agg 15 rows ",
+			data: `{
+"BatchID":"123",
+"Records":[
+	{"Key":"1", "Amount":1.2},
+	{"Key":"2", "Amount":2.2},
+	{"Key":"3", "Amount":3.3},
+	{"Key":"1", "Amount":10.2},
+	{"Key":"1", "Amount":1.2},
+	{"Key":"2", "Amount":2.2},
+	{"Key":"3", "Amount":3.3},
+	{"Key":"1", "Amount":10.2},
+	{"Key":"1", "Amount":1.2},
+	{"Key":"2", "Amount":2.2},
+	{"Key":"3", "Amount":3.3},
+	{"Key":"1", "Amount":10.2},
+	{"Key":"1", "Amount":1.2},
+	{"Key":"2", "Amount":2.2},
+	{"Key":"3", "Amount":3.3}
+]
+}`,
+			expect: map[string]float32{
+				"1": 35.4,
+				"2": 8.8,
+				"3": 13.2,
+			},
+			collector: &recordCollector{collection: map[string]float32{}},
+		},
 	}
 
 	for _, testCase := range testCases {
-		handler := proxy.NewHandler(testCase.collector, reflect.TypeOf(records{}))
+		handler := NewHandler(testCase.collector, reflect.TypeOf(records{}))
 		err := handler.Handle([]byte(testCase.data))
 		if !assert.Nil(t, err, testCase.description) {
 			continue
 		}
 		assert.EqualValues(t, testCase.expect, testCase.collector.collection, testCase.description)
+	}
+
+}
+
+func TestHandler_computeThreadRanges(t *testing.T) {
+
+	var testCases = []struct {
+		length      int
+		divisor     int
+		description string
+		expected    []int
+	}{
+		{
+			length:      4,
+			divisor:     4,
+			expected:    []int{1, 2, 3, 4},
+			description: "trying to divide a slice of 4 elements into 4 parts",
+		},
+		{
+			length:      1,
+			divisor:     4,
+			expected:    []int{1},
+			description: "trying to divide a slice of 1 elements into 4 parts",
+		},
+		{
+			length:      4,
+			divisor:     1,
+			expected:    []int{4},
+			description: "trying to divide a slice of 4 elements into 1 parts",
+		},
+		{
+			length:      4,
+			divisor:     0, // updated to 1 inside fnc
+			expected:    []int{4},
+			description: "trying to divide a slice of 4 elements into 1 parts",
+		},
+		{
+			length:      0,
+			divisor:     4,
+			expected:    []int{},
+			description: "trying to divide a slice of 0 elements into 4 parts",
+		},
+		{
+			length:      3,
+			divisor:     7,
+			expected:    []int{1, 2, 3},
+			description: "trying to divide a slice of 3 elements into 7 parts",
+		},
+		{
+			length:      7,
+			divisor:     3,
+			expected:    []int{2, 4, 7},
+			description: "trying to divide a slice of 7 elements into 3 parts",
+		},
+
+		{
+			length:      31,
+			divisor:     7,
+			expected:    []int{4, 8, 12, 16, 20, 24, 31},
+			description: "trying to divide a slice of 31 elements into 7 parts",
+		},
+	}
+
+	for _, testCase := range testCases {
+		actual := partedSliceUpperBounds(testCase.length, testCase.divisor)
+		assert.EqualValues(t, testCase.expected, actual, testCase.description)
 	}
 
 }
@@ -57,6 +213,7 @@ type (
 
 	records         []*record
 	recordCollector struct {
+		mux        sync.Mutex
 		collection map[string]float32
 	}
 )
@@ -66,6 +223,9 @@ func (c *recordCollector) Collect(rec interface{}) error {
 	if !ok {
 		return fmt.Errorf("expected: %T, but had: %T", aRecord, rec)
 	}
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
 	if len(c.collection) == 0 {
 		c.collection = map[string]float32{}
 	}
