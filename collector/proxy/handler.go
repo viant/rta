58e8419b2df8782a130e9031c1224c4d134fb7a2
pol-mfilipowicz <pol-mfilipowicz@viant-contractors.com>
2024-10-24 01:33:40 +0200

    printing request body for testing

--- collector/proxy/handler.go
+++ collector/proxy/handler.go
@@ -86,7 +86,7 @@ func (h *Handler) Handle(data []byte) error {
 	request := &Request{}
 	request.Records = sliceValuePtr.Interface()
 	if err := gojay.Unmarshal(data, request); err != nil {
-		fmt.Printf("rta-proxy-handler-hanle: unmarshal error: %s", string(data))
+		fmt.Printf("rta-proxy-handler-handle: unmarshal error: %v, data size: %d data: %s", err, len(data), string(data))
 		return err
 	}
 
