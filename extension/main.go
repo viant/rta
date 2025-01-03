3ee58c395c1875446f0b3adfa732d10cd1da290e
gopherus <mfilipowicz@interia.eu>
2025-01-03 12:59:18 +0100

    updated extension

--- extension/main.go
+++ extension/main.go
@@ -2,11 +2,20 @@ package extension
 
 import (
 	"github.com/viant/x"
-	"github.com/viant/xreflect"
 	"reflect"
+	"time"
 )
 
 func init() {
-	aType := x.NewType(reflect.TypeOf(Signal{}), x.WithName("Signal"))
+	aType := x.NewType(reflect.TypeOf(Signal2{}), x.WithName("Signal"))
 	registry.Register(aType)
+	//registry.Register(x.NewType(reflect.TypeOf(Signal2{}), x.WithName("Signal2")))
+}
+
+type Signal2 struct {
+	ID     int         `aerospike:"id,pk" sqlx:"id"`
+	Value  interface{} `aerospike:"value,mapKey" sqlx:"value"`
+	Bucket int         `aerospike:"bucket,arrayIndex,arraySize=144" sqlx:"bucket"`
+	Count  int64       `aerospike:"count,component" sqlx:"count"`
+	At     *time.Time  `aerospike:"-" sqlx:"-"`
 }
