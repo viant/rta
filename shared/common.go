f3b9fbf568dd529e345d06b83b1a6d8bc17b77b5
gopherus <mfilipowicz@interia.eu>
2025-01-22 20:30:36 +0100

    updated error handling

--- shared/common.go
+++ shared/common.go
@@ -1,7 +1,27 @@
 package shared
 
+import (
+	"fmt"
+	"io"
+)
+
 const (
 	Active   = 1
 	InActive = 0
 	Deleted  = -1
 )
+
+// CloseWithErrorHandling closes the closer and handles the error
+func CloseWithErrorHandling(c io.Closer, err *error) {
+	if c == nil {
+		return
+	}
+
+	if cerr := c.Close(); cerr != nil {
+		if err != nil && *err != nil {
+			*err = fmt.Errorf("%w; close error: %v", *err, cerr)
+		} else {
+			*err = cerr
+		}
+	}
+}
